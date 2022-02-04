0825155e94163cf2b0aa49e962668c7f149fb49f
Haris-Javed101 <61041158+Haris-Javed101@users.noreply.github.com>
2022-02-04 18:35:06 +0500

    Update and rename linkedList.cpp to LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+using namespace std;
+
+struct Node{
+		int Info;
+		struct node* Link;
+	};
+	
+class LinkedList{				
+	private:		// by the way, default visibility of member variables is **private** 
+		struct node* START;
+	
+		struct node* CreateNode(){
+				struct Node* Tmp = new struct Node; 
+				return Tmp;
+		} 
+	public:
+		LinkedList(){ 	//constructor function
+			this->START=NULL;
+		}		
+		void InsertNodeAtStart(){
+		
+			struct node* Tmp = this->CreateNode();
+			cout<<"Enter integer value to insert: "; 
+			cin>>Tmp->Info;
+			
+			if(this->START==NULL)
+				Tmp->Link=NULL;
+			else // if list is not empty
+				Tmp->Link=START;
+				
+			this->START=Tmp;
+		}	
+		void DeleteNodeFromEnd(){
+			
+			if(this->START==NULL)
+				cout<<"List is already empty!\n";
+			else{
+				struct node* ToDel = this->START;
+				if(ToDel->Link==NULL){  // if list has only one node
+					delete ToDel;
+					this->START=NULL;
+				}
+				else{  // if list has more than one node		
+					while((ToDel->Link)->Link!=NULL){   // ToDel is previous node to the node to be deleted
+						ToDel=ToDel->Link;
+					}
+					free(ToDel->Link);  // or del toDel->link
+					ToDel->Link=NULL;		
+				}
+				cout<<"Deleted node from end.";
+			}
+		}	
+		void View(){
+			if(this->START==NULL)
+				cout<<"List is empty!";
+			else{
+				struct node* Trvs = START;
+				while(Trvs!=NULL){
+					cout<<Trvs->Info<<" ";
+					Trvs = Trvs->Link;
+				}
+			}
+		}				
+};
+int Menu(){
+	int Ch;
+	cout<<"\n1. Insert node at start";
+	cout<<"\n2. Delete node from end";
+	cout<<"\n3. View list";
+	cout<<"\n4. Exit";
+	cout<<"\n\nPlease, your choice? "; cin>>Ch;
+	return Ch;
+}
+int main(){	
+	LinkedList List;
+	while(1){
+		system("cls");
+		switch(Menu()){
+			case 1: List.InsertNodeAtStart(); break;
+			case 2: List.DeleteNodeFromEnd(); break;
+			case 3: List.View(); break;
+			case 4: return 0; break;
+			default: {
+					cout<<"Invalid Choice!";
+					return 1;
+			} 	
+		}
+		getch();
+	}
+	return 0;
+}
